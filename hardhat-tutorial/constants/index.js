6e4eaf97cc4949bb8c7e5983b7a2ddb10197b8d7
Vinzon <luongvinson@gmail.com>
2022-10-26 15:41:47 -0600

    added api for tokenId

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xcaad32d5C2E6090E40591Ae9128ec1fCA2bc81d5";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://learnweb3-nft-collection-cryptovinzon.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
